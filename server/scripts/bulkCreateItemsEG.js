8f5a7bf8cf6d70d01d24320bfbbd323721190d84
Shejo Joseph <shejojoseph@gmail.com>
2025-06-13 14:31:42 +0200

    typescript refractor index.ts and routes

--- server/scripts/bulkCreateItemsEG.js
+++ server/scripts/bulkCreateItemsEG.js
@@ -1,8 +1,14 @@
-// obtener la data en un archivo CSV 
+// obtener la data en un archivo CSV
 const fs = require('fs');
 const csv =require('csv/sync');
 const db = require('../models/index')
-const items = csv.parse(fs.readFileSync('scripts/input/T1-L-T.csv', 'utf8'), {columns: true, delimiter:',' }); // parse files into an array
+
+
+const items = csv.parse(
+  fs.readFileSync('scripts/input/T1-L-T.csv', 'utf8'),
+  {columns: true, delimiter:',' }
+); // parse files into an array
+
 items.forEach(async (item) => {
   console.log('creating item', item.product_name);
   const price = Number(item.price_bs);
@@ -12,10 +18,10 @@ items.forEach(async (item) => {
   }
 // POST request to server to create item
   const result = await db.Items.create({
-    SupermercadoId: item.supermarket, 
-    item_name: item.product_name, 
-    item_name_toLowerCase: item.product_name_toLowerCase, 
-    item_id: item.product_id, 
+    SupermercadoId: item.supermarket,
+    item_name: item.product_name,
+    item_name_toLowerCase: item.product_name_toLowerCase,
+    item_id: item.product_id,
     price: item.price_bs
   });
   console.log('created item_name successfully')
